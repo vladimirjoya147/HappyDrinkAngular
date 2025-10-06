aa19075c91a2e2cec2462628b1d79ab307437f2d
Marco <marco_x1230@hotmail.com>
2025-10-06 01:20:28 -0500

    Add Cliente model, Clientes component, and service for client management

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { Productos } from './productos/productos';
 import { AuthGuard } from './core/Guards/auth.guard';
 import { NgModule } from '@angular/core';
 
+import { ClientesComponent } from './clientes/clientes.component';
 
 export const routes: Routes = [
     { path: 'login', component: Login},
+    { path: 'clientes', component: ClientesComponent, canActivate: [AuthGuard]},
     { path:'productos', component: Productos, canActivate: [AuthGuard]},
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: '**', redirectTo: 'login' }
