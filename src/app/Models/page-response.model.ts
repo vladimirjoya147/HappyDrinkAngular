c2c27d1697a11780d26416a114728dafa2917b12
Vladimir Joya Rojas <121407957+vladimirjoya147@users.noreply.github.com>
2025-10-08 21:31:10 -0500

    Revert "Add Cliente model, Clientes component, and service for client management"

--- src/app/Models/page-response.model.ts
+++
@@ -1,9 +0,0 @@
-export interface PageResponse<T> {
-    content: T[];
-    page: number;
-    size: number;
-    totalElements: number;
-    totalPages: number;
-    first: boolean;
-    last: boolean;
-}
